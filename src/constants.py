6cda0f0f2d0bd562b8e1695d6a458ffa2a9a8e96
Iñigo Gabirondo López <inigo.ikasle@gmail.com>
2022-07-25 16:53:57 +0200

    Add initial menu and enable 2 programs

--- src/constants.py
+++ src/constants.py
@@ -1,14 +1,20 @@
 ACCESS_TOKEN = "35HX42EZ3SGC7WPDQLV7QFTXGXNV5W4N"
 
 PROGRAM_CODES = {
-    'browser':0
+    'browser':0,
+    'telegram':1,
+    'editor':2
 }
 
 PROGRAM_PATHS = {
-    0:"/usr/bin/google-chrome"
+    0:'/usr/bin/google-chrome',
+    1:'/home/inigo/_insta/Telegram/Telegram',
+    2:'/usr/bin/xed'
 }
 
 PROGRAM_NAMES = {
-    0:"chrome"
+    0:'chrome',
+    1:'telegram',
+    2:'xed'
 
 }
--- src/main.py
+++ src/main.py
@@ -3,11 +3,30 @@ from utils import get_last_message_info
 from constants import ACCESS_TOKEN
 from program_manager import run_action
 
+client = Wit(ACCESS_TOKEN)
+
+def print_menu():
+    print("Choose type of message input:")
+    print("0) Text message")
+    print("1) Voice message")
+    option = int(input("=============\n"))
+    return option
+
+def get_text_message():
+    msg = str(input("Enter text command:"))
+    resp = client.message(msg)
+    return resp
+
 
 def main():
-    client = Wit(ACCESS_TOKEN)
-    resp = client.message('Stop browser')
-    print(resp)
+    option = print_menu()
+
+    if option == 0:
+        resp = get_text_message()
+
+    else:
+        return -1
+
     info = get_last_message_info(resp)
     print(info)
     run_action(info)
